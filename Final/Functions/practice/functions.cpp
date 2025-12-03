81bd1f743b17747943b674bddce2c18c90f641a6
Hamadullah12 <hamadfms05@gmail.com>
2025-12-03 14:06:15 +0800

    problems on functions are solved

--- Final/Functions/practice/functions.cpp
+++ Final/Functions/practice/functions.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
-void print()
+void printHello()
 {
-    cout << "hello world";
+    cout << "hello world"<<endl;
+}
+void printhi()
+{
+    cout << "Starting print hi" << endl;
+    printHello();
+    cout << "Ending Print hi funtion" << endl;
 }
 int main()
 {
-    print();
+    printHello();
+    printhi();
     return 0;
 }
