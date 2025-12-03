02b462bf89612bb76c2c84c914f2200493df07f6
Hamadullah12 <hamadfms05@gmail.com>
2025-12-03 14:15:59 +0800

    done with nested main function problem

--- Final/Functions/practice/functions.cpp
+++ Final/Functions/practice/functions.cpp
@@ -1,18 +1,33 @@
-#include <iostream>
-using namespace std;
-void printHello()
-{
-    cout << "hello world"<<endl;
-}
-void printhi()
-{
-    cout << "Starting print hi" << endl;
-    printHello();
-    cout << "Ending Print hi funtion" << endl;
-}
-int main()
-{
-    printHello();
-    printhi();
-    return 0;
-}
+// Functions: Its a block of code runs when it is called . it is used for reusable code
+// we cannot declare nested function in main
+// #include <iostream>
+// using namespace std;
+// void printHello()
+// {
+//     cout << "hello world" << endl;
+// }
+// void printhi()
+// {
+//     cout << "Starting print hi" << endl;
+//     printHello();
+//     cout << "Ending Print hi funtion" << endl;
+// }
+// int main()
+// {
+//     printHello();
+//     printhi();
+//     return 0;
+// }
+
+// NESTED FUNCTION INSIDE MAIN FUNCTION
+// #include <iostream>
+//     using namespace std;
+// int main()
+// {
+//     void myFunc()
+//     {
+//         cout << "Hello" << endl;
+//     }
+//     myFunc();
+// }
+// so its clear that we cannot defined function in main we just have to call it in a main function
