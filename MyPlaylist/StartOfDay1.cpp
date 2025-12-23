9b842a6c98377f39488cd644b460b232cc19ea75
Hamadullah12 <hamadfms05@gmail.com>
2025-12-23 11:02:04 +0800

    break and continure are discuseed now focused on a complete proper program to practice

--- MyPlaylist/StartOfDay1.cpp
+++ MyPlaylist/StartOfDay1.cpp
@@ -48,10 +48,38 @@
 //     } while (i >= 1);
 // }
 // ========================================================================
-// Day 2 Break and Continue 
-#include<iostream>
-using namespace std;
-int main(){
+// Day 2 Break and Continue
+// Break : When we want to break the loop in runing so we use break, it will exit the loop.
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     for (int i = 0; i < 5; i++)
+//     {
+//         if (i == 2)
+//         {
+//             break;
+//         }
+//         cout << i << endl;
+//     }
+//     return 0;
+// }
+// Continue:THe purpose of using continue keyword is that it will skip the value where continue is applied
+// and complete the loop when the continue value condition comes true so it didnt go down and print the value
+// it directly goes up to the loop . it will not break the loop its use to skip the value which we dont want
+// as in the below program the even numbers are skiped without disturbing the loop
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     for (int i = 0; i < 20; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             continue;
+//         }
+//         cout << i << endl;
+//     }
 
-return 0;
-}
+//     return 0;
+// }
