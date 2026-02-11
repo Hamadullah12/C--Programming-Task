494ab561e5e2b2c543e2fb68f76fb1a732081f89
Hamadullah12 <hamadfms05@gmail.com>
2026-02-11 13:52:36 +0800

    employees data managed with using for loop

--- MyPlaylist/OOPS/Structures.cpp
+++ MyPlaylist/OOPS/Structures.cpp
@@ -11,8 +11,17 @@ struct Employee // here employe is a data type just like int and double and char
 
 int main()
 {
+
+    // SIMPLEE ASSIGNING DATA WITHOUT ARRAYS
+    // Employee Manager;
+    // Manager.id = 234;
+    // Manager.salary = 25000.0;
+    // Manager.department = "Mineral";
+    // cout << "Employee id         :" << Manager.id << endl;
+    // cout << "Employee Salary     :" << Manager.salary << endl;
+    // cout << "Employee Department :" << Manager.department << endl;
     // handling data of more than one employee using array
-    Employee team[3];
+    // Employee team[3];
     // IDS ARE APPOINTED
     // team[0].id = 1;
     // team[1].id = 2;
@@ -26,13 +35,22 @@ int main()
     // team[1].department = "IT";
     // team[2].department = "Controlling";
 
-    // SIMPLEE ASSIGNING DATA WITHOUT ARRAYS
-    // Employee Manager;
-    // Manager.id = 234;
-    // Manager.salary = 25000.0;
-    // Manager.department = "Mineral";
-    // cout << "Employee id         :" << Manager.id << endl;
-    // cout << "Employee Salary     :" << Manager.salary << endl;
-    // cout << "Employee Department :" << Manager.department << endl;
+    // ASSIGNING DATA TO EMPLOYESS USING FOR LOOP
+    Employee team[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Enter Your Id ?" << endl;
+        cin >> team[i].id;
+        cout << "Enter your salary ? " << endl;
+        cin >> team[i].salary;
+        cout << "Enter your department" << endl;
+        cin >> team[i].department;
+    }
+    for (int j = 0; j < 3; j++)
+    {
+        cout << "Your Id is         :" << team[j].id << endl;
+        cout << "Your Salary is     :" << team[j].salary << endl;
+        cout << "Your department is :" << team[j].department << endl;
+    }
     return 0;
 }
